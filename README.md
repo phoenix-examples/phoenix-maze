7c9e5353e5203a952c854f42fd22cbf288c162b7
Pat Paasch <ppaasch@gfxi.com>
2015-09-11 17:47:11 -0500

    WIP

--- README.md
+++ README.md
@@ -1,5 +1,14 @@
 # HelloPhoenix
 
+Trying to port Rawkets (https://github.com/robhawkes/rawkets) to Elixir and Phoenix. FYI - no Twitter login necessary.
+Demo site at http://canarymod16.cloudapp.net/rawkets but I don't always have it running since I think it'll burn throw my spending limit. 
+
+Todos:
+- hits and kill
+- get unneccessary files (mnesia/suffixed ~ files/etc out of repo
+- dont use numbers for message types
+
+
 To start your new Phoenix application:
 
 1. Install dependencies with `mix deps.get`
--- lib/hello_phoenix/update_bullets.ex
+++ lib/hello_phoenix/update_bullets.ex
@@ -16,18 +16,52 @@ defmodule UpdateBullets do
     updated_bullet = %{bullet | x: bullet.x + bullet.vX, y: bullet.y + bullet.vY, age: bullet.age + 1}
     
     cond do 
-      updated_bullet.age > 10 -> 
+      updated_bullet.age > 20 -> 
         Amnesia.transaction do
           bullet |> Bullet.delete
         end
+        #Remove Bullet
         HelloPhoenix.Endpoint.broadcast! "rawkets:game", "13", %{i: bullet.id}
       true -> 
+        #kill player?
+        Amnesia.transaction do
+          selection = Player.where alive == true
+          if selection do
+            alive_players = selection |> Amnesia.Selection.values
+            kill_player(alive_players, updated_bullet)
+          end
+        end
+
         Amnesia.transaction do 
           updated_bullet |> Bullet.write
         end
+        
+        #Update Bullet
         HelloPhoenix.Endpoint.broadcast! "rawkets:game", "12", %{i: updated_bullet.id, x: updated_bullet.x, y: updated_bullet.y} 
     end
+  end
 
+  defp kill_player(players, bullet) do
+      if player[0].id != bullet.id do
+       kill_radius = get_kill_radius(player, bullet)
+       
+       cond do 
+          kill_radius < 10 ->  
+            HelloPhoenix.Endpoint.broadcast! "rawkets:game", "", %{i: player.id}
+            updated_player = %{player | alive: false}
+            Amnesia.transaction do
+              updated_player |> Player.write
+            end
+          _ -> 
+            kill_player(players[h:t], bullet)
+      end
+  end
+
+  defp get_kill_radius(player, bullet) do 
+    dx = bullet.x - player.x
+    dy = bullet.y - player.y
+    dd = (dx * dx) + (dy * dy);
+    :math.sqrt(dd);
   end
 
 end
